017c75a4742ca616bf1f68709ac3e8abf99e7c08
YiBaJiu <steve6211942@yahoo.com.tw>
2020-04-19 15:14:53 +0800

    update server and client

--- app/src/main/java/com/example/newwwdle/Backend.java
+++ app/src/main/java/com/example/newwwdle/Backend.java
@@ -1,4 +1,4 @@
-package com.example.newwwdle;
+package com.example.testclient;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -29,7 +29,6 @@ public class Backend {
     private String CID = "";
     private int write_flag = 0;
     private int KEY = -1;
-    private int status = -1;    // 0 for catch, 1 for
     private int ret_flag = -1;
     private String result = "";
     Backend(){
@@ -45,7 +44,7 @@ public class Backend {
             // TODO Auto-generated method stub
             try{
                 // IP為Server端
-                InetAddress serverIp = InetAddress.getByName("192.168.43.92");
+                InetAddress serverIp = InetAddress.getByName("192.168.43.9");
                 int serverPort = 8888;
                 clientSocket = new Socket(serverIp, serverPort);
                 //取得網路輸出串流
@@ -54,116 +53,103 @@ public class Backend {
                 br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 // 當連線後
                 while (clientSocket.isConnected()) {
-/*
-                    // 取得網路訊息
-                    Log.d("OUTPUTLA","123");
-                    tmp = br.readLine();    //宣告一個緩衝,從br串流讀取值
-                    Log.d("OUTPUTLA",tmp);
-                    // 如果不是空訊息
-                    if(tmp!=null){
-                        // send message from server
-                        bw.write(tmp);
-                        bw.flush();
-                    }
-*/
-
                     if(write_flag == 1){
                         bw.write(send_msg);
                         bw.flush();
-                        status = 0;
-                        if(status == 0){
-                            //Log.d("OUTPUTLA",send_msg);
-                            tmp = br.readLine();
-                            switch(KEY){
-                                case 1:
-                                    if(tmp.equals("No Account")){
-                                        Log.d("OUTPUTLA","NO");
-                                        ret_flag = 1;
-                                        KEY = -1;
-                                        result = tmp;
-                                    }
-                                    else if(tmp.equals("Wrong Password")){
-                                        Log.d("OUTPUTLA","WRONG");
-                                        ret_flag = 1;
-                                        KEY = -1;
-                                        result = tmp;
-                                    }
-                                    else{
-                                        Log.d("OUTPUTLA","SUCCESS");
-                                        ret_flag = 1;
-                                        KEY = -1;
-                                        result = tmp;
-                                    }
-                                    break;
+                        tmp = br.readLine();
+                        switch(KEY){
+                            case 1:
+                                if(tmp.equals("No Account")){
+                                    Log.d("OUTPUTLA","NO");
+                                }
+                                else if(tmp.equals("Wrong Password")){
+                                    Log.d("OUTPUTLA","WRONG");
+                                }
+                                else{
+                                    Log.d("OUTPUTLA","SUCCESS");
+                                }
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
 
-                                case 2:
-                                    Log.d("OUTPUTLA","ANNOUNCE");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
-                                case 3:
-                                    Log.d("OUTPUTLA","ALL STATUS");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
-                                case 4:
-                                    Log.d("OUTPUTLA","IMEI CHECK");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
-                                case 5:
-                                    Log.d("OUTPUTLA", "IMEI ADD");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
-                                case 6:
-                                    Log.d("OUTPUTLA","ROLL CALL CHECK(TODAY)");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
+                            case 2:
+                                Log.d("OUTPUTLA","ANNOUNCE");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
 
-                                case 7:
-                                    Log.d("OUTPUTLA","ROLL CALL CHECK(ALL)");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
-                                case 8:
-                                    Log.d("OUTPUTLA","ROLL CALL UPDATE");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
+                            case 3:
+                                Log.d("OUTPUTLA","ALL STATUS");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
 
-                                case 9:
-                                    Log.d("OUTPUTLA","ADD ANNOUNCEMENT");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
+                            case 4:
+                                Log.d("OUTPUTLA","IMEI CHECK");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
+                            case 5:
+                                Log.d("OUTPUTLA", "IMEI ADD");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
 
-                                case 10:
-                                    Log.d("OUTPUTLA","ENABLE TO ROLL CALL");
-                                    ret_flag = 1;
-                                    KEY = -1;
-                                    result = tmp;
-                                    break;
-                            }
-                            while(true){
-                                // stop
-                            }
-                            //Log.d("OUTPUTLA",tmp);
+                            case 6:
+                                Log.d("OUTPUTLA","ROLL CALL CHECK(TODAY)");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
+
+                            case 7:
+                                Log.d("OUTPUTLA","ROLL CALL CHECK(ALL)");
+                                Log.d("OUTPUTLA",tmp);
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
+
+                            case 8:
+                                Log.d("OUTPUTLA","ROLL CALL UPDATE");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
+
+                            case 9:
+                                Log.d("OUTPUTLA","ADD ANNOUNCEMENT");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
+
+                            case 10:
+                                Log.d("OUTPUTLA","ENABLE TO ROLL CALL");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
+
+                            case 11:
+                                Log.d("OUTPUTLA","GET TEACHER GPS");
+                                KEY = -1;
+                                result = tmp;
+                                write_flag = 0;
+                                break;
                         }
-                        write_flag = 0;
-
+                        ret_flag = 1;
                     }
 
+
                 }
+
+
             }catch(Exception e){
                 //當斷線時會跳到catch,可以在這裡寫上斷開連線後的處理
                 e.printStackTrace();
@@ -204,44 +190,39 @@ public class Backend {
 
         switch(key){
             case 2: // key = 2, input: CID and return all class announce (time,title,intent) : function(KEY=2,CID)
-                sendMessage("catch,-1,-1,-1,"+input1+",-1",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                sendMessage("catch,-1,-1,-1,"+input1+",3",key);
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return result;
 
             case 3: // key = 3, (Teacher)input: CID and return class roll call status: function(KEY=3,CID), send a key to distinguish
                 sendMessage("catch,-1,-1,-1,"+input1+",2",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return result;
 
             case 4: // key = 4, input: IMEI and return a result(Enable or Disable to login) : function(KEY=4,IMEI)
                 sendMessage("catch,"+input1+",-1,-1,-1,-1",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return result;
 
             case 5: // key = 5, input: IMEI and add this IMEI to database : function(KEY=5,IMEI)
                 sendMessage("add,"+input1+",-1,-1,-1,-1,-1",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return "NothingToDo";
+
+            case 11: // key = 11, input: CID and get teacher's GPS
+                sendMessage("catch,-1,-1,-1,"+input1+",4",key);
+                lock();
+                initial();
+                Log.d("OUTPUTLA",result);
+                return result;
         }
         return "";
     }
@@ -252,41 +233,30 @@ public class Backend {
         switch (key){
             case 1: // key = 1, input: SID,passwd -> Login and return CID cname name type ctime : function(KEY=1,ID)
                 sendMessage("catch,-1,"+input1+","+input2+",-1,-1",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return result;
 
             case 6: // key = 6, input: SID,CID and (student)get roll call status(today) : function(KEY=6,SID,CID)
                 sendMessage("catch,-1,"+input1+",-1,"+input2+",0",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return result;
 
             case 7: // key = 7, input: SID,CID and (student)get all roll call of this class : function(KEY=7,SID,CID)
+                //result = "";
                 sendMessage("catch,-1,"+input1+",-1,"+input2+",1",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
-                Log.d("OUTPUTLA",result);
+                lock();
+                initial();
+                Log.d("OUTPUTLA","HERE");
                 return result;
 
             case 8: // key = 8, input: SID,CID and update roll call in database : function(KEY=8,SID,CID)
                 sendMessage("add,-1,-1,"+input1+","+input2+",-1,-1",key);
-                while(ret_flag == 0){
-                    // enable to return string
-
-                }
-                ret_flag = -1;
+                lock();
+                initial();
                 Log.d("OUTPUTLA",result);
                 return "NothingToDo";
         }
@@ -298,11 +268,8 @@ public class Backend {
         // key = 9, input: cid, title, announce and add an announce in database : function(KEY=9,CID,title,announce)
         if (key == 9) {
             sendMessage("add,-1,-1,-1," + cid + "," + title + "," + announce + ",-1",key);
-            while(ret_flag == 0){
-                // enable to return string
-
-            }
-            ret_flag = -1;
+            lock();
+            initial();
             Log.d("OUTPUTLA",result);
             return "NothingToDo";
         }
@@ -314,11 +281,8 @@ public class Backend {
             // add : [IMEI, start/stop, ID , CID, announce, GPS]
             // key = 10, input: cid, startSign, GPS and update status of roll_call_sign : function(KEY=10,CID,startSign,GPS1,GPS2)
             sendMessage("add,-1,"+start_stop+",-1,"+cid+",-1,"+gps_1+","+gps_2,key);
-            while(ret_flag == 0){
-                // enable to return string
-
-            }
-            ret_flag = -1;
+            lock();
+            initial();
             Log.d("OUTPUTLA",result);
             return "NothingToDo";
         }
@@ -332,4 +296,25 @@ public class Backend {
         write_flag = 1;
     }
 
+    private void lock(){
+        // lock until finish passing message
+        while(true){
+            //
+            try {
+                this.thread.sleep(100);
+            }catch (InterruptedException e){
+
+            }
+
+            if(ret_flag == 1) break;
+        }
+    }
+
+    private void initial(){
+        // initial variable
+        ret_flag = -1;
+        KEY = -1;
+        write_flag = 0;
+        send_msg = "";
+    }
 }
